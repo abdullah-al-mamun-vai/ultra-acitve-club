73af4d701b5448a1fb403c82e14fbda8316192a0
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-09-29 02:10:16 +0600

    break state added

--- src/Componant/Intro/Intro.js
+++ src/Componant/Intro/Intro.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Intro = ({ cart }) => {
+    const [relax, setRelax] = useState(0)
+    const handleBreak = (second) => {
+        setRelax(second)
+
+    }
     return (
         <div className='sticky top-0 mx-2'>
             <div>
@@ -25,11 +30,11 @@ const Intro = ({ cart }) => {
                 <h2 className='capitalize text-xl font-bold mx-4 my-8'>add a break</h2>
                 <div>
                     <div className='flex justify-evenly bg-slate-400 rounded-md py-3 mx-2'>
-                        <button className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>10s</button>
-                        <button className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>20s</button>
-                        <button className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>30s</button>
-                        <button className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>40s</button>
-                        <button className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>50s</button>
+                        <button onClick={() => handleBreak(10)} className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>10s</button>
+                        <button onClick={() => handleBreak(20)} className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>20s</button>
+                        <button onClick={() => handleBreak(30)} className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>30s</button>
+                        <button onClick={() => handleBreak(40)} className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>40s</button>
+                        <button onClick={() => handleBreak(50)} className='rounded-full focus:bg-slate-600 rounded-full p-2 text-white'>50s</button>
                     </div>
                 </div>
             </div>
@@ -37,11 +42,11 @@ const Intro = ({ cart }) => {
                 <h2 className='capitalize text-xl font-bold mx-4 my-8'>exercise details</h2>
                 <div className='flex justify-evenly bg-amber-100 py-4 mt-8'>
                     <h3 className='text-lg font-semibold capitalize'>exercise time</h3>
-                    <p className='text-slate-600'>{cart}</p>
+                    <p className='text-slate-600'>{cart}s</p>
                 </div>
                 <div className='flex justify-evenly bg-amber-100 py-4'>
                     <h3 className='text-lg font-semibold capitalize'>break time</h3>
-                    <p className='text-slate-600'>50s</p>
+                    <p className='text-slate-600'>{relax}s</p>
                 </div>
             </div>
             <button className='btn w-full bg-green-500 my-8'>activity complete</button>
